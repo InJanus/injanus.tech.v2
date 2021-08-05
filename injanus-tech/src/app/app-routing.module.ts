330e82c82f3504fe6416ede2a309ebc55b9afb65
InJanus <briannculberson@gmail.com>
2021-08-04 20:28:06 -0400

    init

---
+++ injanus-tech/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path : '', redirectTo : 'main' },
+  { path : 'main', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule)}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ injanus-tech/src/app/layout/home-page/home-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageViewComponent } from './home-page-view/home-page-view.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePageViewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomePageModule { }
